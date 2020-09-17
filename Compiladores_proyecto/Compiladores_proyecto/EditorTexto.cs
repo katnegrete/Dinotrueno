304f9baa24d83d6400af7d72d2e0d71717efbc03
Katia <kati_cobarrubias@hotmail.com>
2020-09-17 16:24:20 -0600

    Cambios de clases

---
+++ Compiladores_proyecto/Compiladores_proyecto/EditorTexto.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Compiladores_proyecto
+{
+    public class EditorTexto
+    {
+        //Variables globales de la clase
+        public string ruta_archivo; //Ruta del archivo actual
+        public bool band_guardado; //Bandera para determinar si los datos están guardados al cerrar el archivo
+		public OpenFileDialog open;
+		public SaveFileDialog save;
+
+		public void crea_archivo()
+		{
+			FileStream fs;
+			// Abre la ventana para crear un archivo.
+			save = new SaveFileDialog();
+			save.Filter = "Text Files | *.txt"; //Especifica que solo se buscan archivos de texto
+			if (save.ShowDialog() == DialogResult.OK) //Si ya se presionó "OK"
+			{
+				ruta_archivo = save.FileName; //Setea el string de la ruta
+				fs = File.Create(save.FileName); //Crea el archivo
+				fs.Close();
+			}
+		}
+
+		public void abre_archivo()
+		{
+			// Abre la ventana para abrir un archivo.
+			open = new OpenFileDialog();
+			open.Filter = "Text Files | *.txt"; //Especifica que solo se buscan archivos de texto
+			if (open.ShowDialog() == DialogResult.OK) //Si ya se presionó "OK"
+			{
+				ruta_archivo = open.FileName; //Setea el string de la ruta
+				band_guardado = true;
+			}
+		}
+
+		public void guarda_archivo(string texto_a_guardar)
+		{
+			File.WriteAllText(ruta_archivo, texto_a_guardar);
+			band_guardado = true;
+		}
+
+		public void cierra_archivo()
+		{
+			ruta_archivo = ""; // Resetea la ruta del archivo
+			band_guardado = true; // Resetea la bandera
+		}
+	}
+}
--- Compiladores_proyecto/Compiladores_proyecto/Form_Menu.cs
+++ Compiladores_proyecto/Compiladores_proyecto/Form_Menu.cs
@@ -14,8 +14,7 @@ namespace Compiladores_proyecto
 	public partial class Form_Menu : Form
 	{
 		// Variables globales
-		string ruta_archivo; //Ruta del archivo actual
-		bool band_guardado; //Bandera para determinar si los datos están guardados al cerrar el archivo
+		EditorTexto editor;
 
 		public Form_Menu()
 		{
@@ -25,14 +24,15 @@ namespace Compiladores_proyecto
 		// Se inicializan todas las variables para usar desde 0
         private void Form1_Load(object sender, EventArgs e)
         {
-			Text_Box.Enabled = false;					// Desactivados porque 
-			guardarToolStripMenuItem.Enabled = false;   // no hay ningun archivo 
-			cerrarToolStripMenuItem.Enabled = false;    // abierto.
-			ruta_archivo = ""; // Cadena vacia preparada para usarse en abrir o crear nuevo archivo.
-			band_guardado = true; // En true porque no se ha modificado nada, lo que significa que no hay cambios.
-			openFileDialog = new OpenFileDialog();
-			saveFileDialog = new SaveFileDialog();
-        }
+			editor = new EditorTexto(); // Inicializa el editor de texto (lo basico)
+			editor.open = new OpenFileDialog();
+			editor.save = new SaveFileDialog();
+
+			editor.ruta_archivo = "";  // Cadena vacia preparada para usarse en abrir o crear nuevo archivo.
+			editor.band_guardado = true; // En true porque no se ha modificado nada, lo que significa que no hay cambios.
+
+			resetea_controles(); // Inicializa los controles
+		}
 
 		// LISTA DE ACCIONES DE LA PESTAÑA "ARCHIVO" -------------------------------------------------------- |
         private void Opciones_Archivo(object sender, ToolStripItemClickedEventArgs e)
@@ -41,123 +41,79 @@ namespace Compiladores_proyecto
             {
 				// Crea un nuevo archivo vacio y lo abre automaticamente.
 				case "nuevo":
-					if (ruta_archivo != "") // Si ya hay un archivo abierto
+					if (!editor.band_guardado) // Si ya hay un archivo abierto con cambios sin guardar
                     {
 						// Pregunta si quiere confirmar porque si ya hay un archivo abierto, se cerrará
-						DialogResult resultado = MessageBox.Show("Un archivo está actualmente abierto.\nSi creas uno nuevo, se cerrará el actual perdiendo los datos no guardados.\n\n¿Continuar?", "Advertencia", MessageBoxButtons.OKCancel);
+						DialogResult resultado = MessageBox.Show("El archivo actual tiene cambios sin guardar.\nSi creas uno nuevo, se cerrará el actual perdiendo los datos no guardados.\n\n¿Continuar?", "Advertencia", MessageBoxButtons.OKCancel);
 						if(resultado == DialogResult.OK) // Si se seleccionó "OK"
                         {
-							cierra_archivo();
-							crea_archivo();
+							// Primero cierra el archivo
+							editor.cierra_archivo();
+							resetea_controles();
+
+							// Despues crea
+							editor.crea_archivo();
+							activa_controles();
 						}
                     }
                     else // No hay un archivo abierto, simplemente crea uno nuevo
                     {
-						crea_archivo();
+						editor.crea_archivo();
+						activa_controles();
 					}
 					break;
 				// Abre un archivo ya existente borrando y descartando todo lo que esté en ese momento.
 				case "abrir":
-					if (ruta_archivo != "") // Si ya hay un archivo abierto
+					if (!editor.band_guardado) // Si ya hay un archivo abierto con cambios sin guardar
 					{
 						// Pregunta si quiere confirmar porque si ya hay un archivo abierto, se cerrará
-						DialogResult resultado = MessageBox.Show("Un archivo está actualmente abierto.\nSi abres otro, se cerrará el actual perdiendo los datos no guardados.\n\n¿Continuar?", "Advertencia", MessageBoxButtons.OKCancel);
+						DialogResult resultado = MessageBox.Show("El archivo actual tiene cambios sin guardar.\nSi abres otro, se cerrará el actual perdiendo los datos no guardados.\n\n¿Continuar?", "Advertencia", MessageBoxButtons.OKCancel);
 						if (resultado == DialogResult.OK) // Si se seleccionó "OK"
 						{
-							cierra_archivo();
-							abre_archivo();
+							// Primero cierra
+							editor.cierra_archivo();
+							resetea_controles();
+
+							// Despues abre
+							editor.abre_archivo();
+							Text_Box.LoadFile(editor.open.FileName, RichTextBoxStreamType.PlainText); //Carga el texto del archivo en el Text_Box
+							activa_controles();
 						}
 					}
 					else // No hay un archivo abierto, simplemente abre otro
 					{
-						abre_archivo();
+						editor.abre_archivo();
+						Text_Box.LoadFile(editor.open.FileName, RichTextBoxStreamType.PlainText); //Carga el texto del archivo en el Text_Box
+						activa_controles();
 					}
 					break;
 				// Sobreescribe el archivo actual con lo que haya en el Text_Box.
 				case "guardar":
-					guarda_archivo();
+					editor.guarda_archivo(Text_Box.Text); // Le envia el texto actual para guardarlo.
 					break;
 				// Cierra un archivo actual, si se hicieron cambios y no se guardaron, se perderán, desactiva controles necesarios
 				case "cerrar":
-					if(band_guardado == false) // Si no se han guardado cambios
+					if(!editor.band_guardado) // Si no se han guardado cambios
                     {
 						// Pregunta si quiere confirmar porque si lo cierra no se quedarán guardados los cambios recientes
-						DialogResult resultado = MessageBox.Show("Hay cambios sin guardar.\nSi cierras el archivo, se perderán los cambios.\n\n¿Continuar?", "Advertencia", MessageBoxButtons.OKCancel);
+						DialogResult resultado = MessageBox.Show("El archivo actual tiene cambios sin guardar.\nSi cierras el archivo, se perderán los cambios.\n\n¿Continuar?", "Advertencia", MessageBoxButtons.OKCancel);
 						if (resultado == DialogResult.OK) // Si se seleccionó "OK".
                         {
-							cierra_archivo();
-                        }
+							editor.cierra_archivo();
+							resetea_controles();
+						}
                     }
                     else // No hay cambios sin guardar, todo bien
                     {
-						cierra_archivo();
+						editor.cierra_archivo();
+						resetea_controles();
 					}
-					
 					break;
 			}
 		}
 
 		// -<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>- FUNCIONES -<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>-
-
-
-		// Funciones de ARCHIVOS -|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
-		void crea_archivo()
-        {
-			// Activa controles correspondientes.
-			Text_Box.Enabled = true;
-			guardarToolStripMenuItem.Enabled = true;
-			cerrarToolStripMenuItem.Enabled = true;
-
-
-			// Abre la ventana para crear un archivo.
-			saveFileDialog = new SaveFileDialog();
-			saveFileDialog.Filter = "Text Files | *.txt"; //Especifica que solo se buscan archivos de texto
-			if(saveFileDialog.ShowDialog() == DialogResult.OK) //Si ya se presionó "OK"
-            {
-				ruta_archivo = saveFileDialog.FileName; //Setea el string de la ruta
-				File.Create(saveFileDialog.FileName); //Crea el archivo
-            }
-
-			// Muestra el nombre del archivo actual en la ventana nomas porque se ve chido
-			this.Text = obten_nombre_archivo_actual(ruta_archivo);
-		}
-		void abre_archivo() 
-		{
-			// Activa controles correspondientes.
-			Text_Box.Enabled = true;
-			guardarToolStripMenuItem.Enabled = true;
-			cerrarToolStripMenuItem.Enabled = true;
-			
-			// Abre la ventana para abrir un archivo.
-			openFileDialog = new OpenFileDialog();
-			openFileDialog.Filter = "Text Files | *.txt"; //Especifica que solo se buscan archivos de texto
-			if (openFileDialog.ShowDialog() == DialogResult.OK) //Si ya se presionó "OK"
-			{
-				ruta_archivo = openFileDialog.FileName; //Setea el string de la ruta
-				Text_Box.LoadFile(openFileDialog.FileName, RichTextBoxStreamType.PlainText); //Carga el texto del archivo en el Text_Box
-				band_guardado = true;
-			}
-
-			// Muestra el nombre del archivo actual en la ventana nomas porque se ve chido
-			this.Text = obten_nombre_archivo_actual(ruta_archivo);
-		}
-		void guarda_archivo()
-        {
-			File.WriteAllText(ruta_archivo, Text_Box.Text);
-			band_guardado = true;
-		}
-		void cierra_archivo()
-        {
-			ruta_archivo = ""; // Resetea la ruta del archivo
-			band_guardado = true; // Resetea la bandera
-			Text_Box.Clear(); // Vacia el Text_Box
-			this.Text = "Analizador Léxico-Sintáctico";
-
-			// Desabilita controles correspondientes.
-			Text_Box.Enabled = false;
-			guardarToolStripMenuItem.Enabled = false;
-			cerrarToolStripMenuItem.Enabled = false;
-		}
+		
 		string obten_nombre_archivo_actual(string s)
 		{
 			string actual = "";
@@ -179,7 +135,28 @@ namespace Compiladores_proyecto
 
 		private void cambios_realizados(object sender, EventArgs e)
 		{
-			band_guardado = false;
+			editor.band_guardado = false;
+		}
+
+		public void resetea_controles()
+        {
+			Text_Box.Clear(); // Vacia el Text_Box
+			this.Text = "Analizador Léxico-Sintáctico";
+
+			Text_Box.Enabled = false;
+			guardarToolStripMenuItem.Enabled = false;
+			cerrarToolStripMenuItem.Enabled = false;
+		}
+
+		public void activa_controles()
+        {
+			// Muestra el nombre del archivo actual en la ventana nomas porque se ve chido
+			this.Text = obten_nombre_archivo_actual(editor.ruta_archivo);
+			editor.band_guardado = true;
+
+			Text_Box.Enabled = true;
+			guardarToolStripMenuItem.Enabled = true;
+			cerrarToolStripMenuItem.Enabled = true;
 		}
 	}
 }
